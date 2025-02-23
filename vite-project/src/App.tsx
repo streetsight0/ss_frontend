bb83cfdc25e81ee756b927200ef7ba7152f9e609
venkatesh <pothamsettyvenky003@gmail.com>
2025-02-23 04:28:02 -0800

    scrum128#: created forms and storing the data back to mongodbe

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './register';
 import Login from './login';
 import Home from './home';
+import BillBoards from './billboards';
 
 const App: React.FC = () => {
     return (
@@ -12,6 +13,7 @@ const App: React.FC = () => {
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/home" element={<Home />} />
+                <Route path ="/billBoards" element={<BillBoards />} />
             </Routes>
         </Router>
     );
--- vite-project/src/home.tsx
+++ vite-project/src/home.tsx
@@ -1,5 +1,6 @@
 
 import "./home.css"
+
 const Home = () => {
     const token = localStorage.getItem('jwt_token');  // Retrieve token from localStorage
 
