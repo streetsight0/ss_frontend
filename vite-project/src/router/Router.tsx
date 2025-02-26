f47cd49e93a09dc41d45f791c9017b0c97a24609
venkatesh <pothamsettyvenky003@gmail.com>
2025-02-25 22:35:28 -0800

    scrum128#: created forms and storing the data back to mongodbe

--- vite-project/src/router/Router.tsx
+++ vite-project/src/router/Router.tsx
@@ -5,6 +5,7 @@ import Register from "../pages/Register/register";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Sidebar from "../components/navigation/Sidebar";
 import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
+import Billboards from "../billboards";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,15 @@ const router = createBrowserRouter([
         element: (
           <ProtectedRoute>
             <Home />
+          </ProtectedRoute>
+        ),
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/billBoards",
+        element: (
+          <ProtectedRoute>
+            <Billboards />
           </ProtectedRoute>
         ),
         errorElement: <ErrorPage />,
