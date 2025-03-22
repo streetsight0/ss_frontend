1153d2de700c217a2ad4f163736f5e1ffca358b2
venkatesh <pothamsettyvenky003@gmail.com>
2025-03-21 21:38:07 -0700

    fix:commented unused code

--- vite-project/src/components/Popup/Popup.tsx
+++ vite-project/src/components/Popup/Popup.tsx
@@ -1,32 +1,32 @@
-import React from "react";
-import "./Popup.css";
+// import React from "react";
+// import "./Popup.css";
 
-interface PopupProps {
-  message: string;
-  duration?: number; 
-  onClose: () => void;
-}
+// interface PopupProps {
+//   message: string;
+//   duration?: number; 
+//   onClose: () => void;
+// }
 
-const Popup: React.FC<PopupProps> = ({ message, onConfirm, onCancel }) => {
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     onCancel(); 
-  //   }, duration);
+// const Popup: React.FC<PopupProps> = ({ message, onConfirm, onCancel }) => {
+//   // useEffect(() => {
+//   //   const timer = setTimeout(() => {
+//   //     onCancel(); 
+//   //   }, duration);
 
-    return () => clearTimeout(timer); 
-  }, [duration, onClose]);
+//     return () => clearTimeout(timer); 
+//   }, [duration, onClose]);
 
-  return (
-    <div className="popup-overlay">
-      <div className="popup">
-        <p>{message}</p>
-        <div>
-          <button onClick={onConfirm}>Delete</button>
-          <button onClick={onCancel}>Cancel</button>
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="popup-overlay">
+//       <div className="popup">
+//         <p>{message}</p>
+//         <div>
+//           <button onClick={onConfirm}>Delete</button>
+//           <button onClick={onCancel}>Cancel</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Popup;
+// export default Popup;
