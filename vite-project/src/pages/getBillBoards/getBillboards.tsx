11f1a98a8867260c04cfd8fb2f0335747d79c72e
venkatesh <pothamsettyvenky003@gmail.com>
2025-03-27 17:57:08 -0700

    removed unuses variables

--- vite-project/src/pages/getBillBoards/getBillboards.tsx
+++ vite-project/src/pages/getBillBoards/getBillboards.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import AvailableBillBoards from "../AvailableBillBoards/AvailableBillBoards";
 import AllBillBoards from "../AllBillBoards/AllBillBoards";
 import Loader from "../../components/Loader/Loader"; 
